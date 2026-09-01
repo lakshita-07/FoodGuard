39ea3f273144f05696df7826b0e3e0683cff44bc
lakshita <232165204+lakshita-07@users.noreply.github.com>
2026-09-01 23:49:09 +0530

    connect backend to frontend API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("`${import.meta.env.VITE_API_URL}/predict`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
