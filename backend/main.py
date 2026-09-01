d11cd2a26ea712778fa5865fea17140ba988e31b
lakshita <232165204+lakshita-07@users.noreply.github.com>
2026-09-01 23:32:51 +0530

    update cors for production

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ load_dotenv()
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://food-guard-smoky.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
